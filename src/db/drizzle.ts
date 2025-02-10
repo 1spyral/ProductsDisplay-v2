52d1716baed799b4cb44eaee580ed4cc3a1181cd
1spyral <83098914+1spyral@users.noreply.github.com>
2025-02-10 18:55:03 -0500

    Edit SQL connection details
    Changed pgSQL provider to Supabase

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,16 +1,11 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
-import * as schema from './schema';
 import dotenv from 'dotenv';
 import postgres from "postgres";
 
 dotenv.config();
 
-export const client = postgres({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT as string),
-    max: 1
-})
-export const db = drizzle(client, { schema });
+const connectionString = process.env.DATABASE_URL as string
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client);

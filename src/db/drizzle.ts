94a776aafadf412c8164d9289acd784443046f06
Luke Zhan <luke.zhan@uwaterloo.ca>
2025-10-04 19:49:27 -0400

    Use global connection pooler

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -3,10 +3,29 @@ import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL as string;
 
-const client = postgres(connectionString, {
-    prepare: false,
-    max: 5,
-    idle_timeout: 10,
-});
+declare global {
+    var postgresClient: ReturnType<typeof postgres> | undefined;
+}
+
+let client: ReturnType<typeof postgres>;
+
+if (process.env.NODE_ENV === "production") {
+    client = postgres(connectionString, {
+        prepare: false,
+        max: 5,
+        idle_timeout: 10,
+        max_lifetime: 60 * 30,
+    });
+} else {
+    if (!global.postgresClient) {
+        global.postgresClient = postgres(connectionString, {
+            prepare: false,
+            max: 1,
+            idle_timeout: 10,
+            max_lifetime: 60 * 30,
+        });
+    }
+    client = global.postgresClient;
+}
 
 export const db = drizzle(client);

6b8eca0dfe923108deb9ea82b80cb5e4ead755a8
Luke Zhan <luke.zhan@uwaterloo.ca>
2025-12-14 16:35:54 -0500

    Allow 100 and 75 as image qualities

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     allowedDevOrigins: ["http://localhost:3000"],
+    images: {
+        qualities: [100, 75],
+    },
     experimental: {
         serverActions: {
             bodySizeLimit: "4mb",

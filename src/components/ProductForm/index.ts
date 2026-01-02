48bc5199109b2fd1c23568b24ce6e77749af8fd5
Luke Zhan <luke.zhan@uwaterloo.ca>
2026-01-02 17:53:55 -0500

    Create ProductPriceField in ProductForm

--- src/components/ProductForm/index.ts
+++ src/components/ProductForm/index.ts
@@ -1,7 +1,11 @@
 // Shared form components
 export { default as ProductFormModal } from "./ProductFormModal";
 export { default as ProductIdField } from "./ProductIdField";
-export { ProductNameField, ProductDescriptionField } from "./ProductTextFields";
+export {
+    ProductNameField,
+    ProductDescriptionField,
+    ProductPriceField,
+} from "./ProductTextFields";
 export { ProductCategoryField } from "./ProductCategoryField";
 export {
     ProductToggleField,

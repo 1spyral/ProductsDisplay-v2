e152db04e0c5e2b799296ee7acf5b78464b3e3d6
Luke Zhan <luke.zhan@uwaterloo.ca>
2026-02-18 12:02:07 -0500

    Add header to All Products pseudocategory

--- src/app/(app)/category/page.tsx
+++ src/app/(app)/category/page.tsx
@@ -11,5 +11,19 @@ export const metadata: Metadata = {
 export default async function AllCategoryPage() {
   const data = await getProducts();
 
-  return <ProductList products={data} />;
+  return (
+    <div className="min-h-full bg-gray-50">
+      <div className="mb-2 border-b-4 border-slate-700 bg-white px-4 py-6">
+        <h1 className="mb-2 text-3xl font-bold tracking-wide text-gray-900 uppercase">
+          All Products
+        </h1>
+        <p className="text-gray-700">
+          {data.length} {data.length === 1 ? "product" : "products"} available
+        </p>
+      </div>
+      <div className="px-4 py-2 sm:px-8">
+        <ProductList products={data} />
+      </div>
+    </div>
+  );
 }
